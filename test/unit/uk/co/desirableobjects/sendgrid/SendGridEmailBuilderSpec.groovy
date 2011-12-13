7d12e0d56ea5f5dbee7ffc5a2b6d501bf8c5e84f
Antony Jones <ant@bronx.(none)>
2011-12-13 10:18:05 +0000

    Improving SendGridEmailBuilder

--- test/unit/uk/co/desirableobjects/sendgrid/SendGridEmailBuilderSpec.groovy
+++ test/unit/uk/co/desirableobjects/sendgrid/SendGridEmailBuilderSpec.groovy
@@ -48,4 +48,22 @@ class SendGridEmailBuilderSpec extends Specification {
 
     }
 
+    def 'Builder can take a recipient with a display name'() {
+
+        given:
+            SendGridEmailBuilder builder = SendGridEmailBuilder.from('antony@example.com').subject('Hello There').withHtml("<h1>What's up?</h1>")
+
+        when:
+            builder.to('antony@example.net')
+
+        then:
+            SendGridEmail email = builder.build()
+            email.recipient == 'antony@example.net'
+            email.toName == 'antony@example.net'
+            Map emailParameters = email.toMap()
+            emailParameters.recipient ==
+
+    }
+
+
 }
