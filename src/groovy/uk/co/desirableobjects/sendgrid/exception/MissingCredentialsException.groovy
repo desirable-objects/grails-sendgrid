6d8e43c8ff91eedc6b593aa864e9b3809a987ea7
Antony Jones <ant@bronx.(none)>
2011-12-31 14:18:40 +0000

    Docs branch

--- src/groovy/uk/co/desirableobjects/sendgrid/exception/MissingCredentialsException.groovy
+++
@@ -1,9 +0,0 @@
-package uk.co.desirableobjects.sendgrid.exception
-
-class MissingCredentialsException extends RuntimeException {
-
-    MissingCredentialsException() {
-        super("Missing login credentials for SendGrid")
-    }
-
-}
