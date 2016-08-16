b2aeafb1c08f2f50ffb87f3e349f3c8b99c8af81
Roberto Perez <roberto.perez@tollfreeforwarding.com>
2016-08-16 16:56:36 -0700

    removed build folder

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '2.0')
-        classNode.putNodeMetaData('projectName', 'grails-sendgrid-grails2')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
